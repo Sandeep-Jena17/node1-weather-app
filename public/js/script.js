d26610362b4d1a0f4ea219176ad9622f3069e883
sandeep jena <jenasandeep595@gmail.com>
2021-01-21 16:31:37 +0530

    Remove unnessery console

--- public/js/script.js
+++ public/js/script.js
@@ -10,18 +10,18 @@ const message2=document.querySelector("#message2");
 
 searchForm.addEventListener("submit",(e)=>{
     e.preventDefault();
-    console.log(`submt`);
+    //console.log(`submt`);
     const location=searchInput.value;
     messageOne.textContent="Loading...";
     fetch(`http://localhost:3001/weather?address=${location}`).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             messageOne.textContent=data.error;
-            console.log(data.error);
+            //console.log(data.error);
         }else{
             messageOne.textContent=data.location;
             message2.textContent=data.forcast;            console.log(data.location);
-            console.log(data.forcast)
+            //console.log(data.forcast)
         }
 
     })
